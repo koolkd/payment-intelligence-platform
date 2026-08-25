8ffd2de00814c1d6a2cb136e1ba72ebbe459a3ab
koolkd <kedarkulkarni239164@gmail.com>
2026-08-25 11:09:13 +0530

    add payment lookup by payment id for Kafka processing

--- distributed-payment-processing/src/main/java/com/payment/platform/repository/PaymentRepository.java
+++ distributed-payment-processing/src/main/java/com/payment/platform/repository/PaymentRepository.java
@@ -3,5 +3,9 @@ package com.payment.platform.repository;
 import com.payment.platform.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    Optional<Payment> findByPaymentId(String paymentId);
 }
